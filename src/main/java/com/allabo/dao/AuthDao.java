0c75a00434cff238f835bbc110a8d2825726fcea
Woonani <nada3377@yonsei.ac.kr>
2023-12-13 19:12:08 +0900

    feat : 로그인 로직변경 (토큰인증추가, 유저정보 반환)
    오류가 나는 VO변수 임시 주석처리

--- src/main/java/com/allabo/dao/AuthDao.java
+++ src/main/java/com/allabo/dao/AuthDao.java
@@ -1,9 +1,14 @@
 package com.allabo.dao;
 
+import java.util.HashMap;
+
 import com.allabo.vo.UsersVO;
 
 public interface AuthDao {
 	public int signup(UsersVO usersVO);
 
 	public int login(UsersVO usersVO);
+	
+	public HashMap<String, String> loginUser(String email);
+	
 }
