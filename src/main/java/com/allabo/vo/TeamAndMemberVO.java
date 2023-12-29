6341b8344a4f03533385d3a13ce71493a8331113
Woonani <nada3377@yonsei.ac.kr>
2023-12-29 12:38:56 +0900

    feat : vo 수정
    변수 추가, 상속 추가

--- src/main/java/com/allabo/vo/TeamAndMemberVO.java
+++ src/main/java/com/allabo/vo/TeamAndMemberVO.java
@@ -8,10 +8,11 @@ public class TeamAndMemberVO {
 	private String teamName; //team_name;
 	private String createdAt; //created_at;
 	private String description;
-	// join문으로 teamList를 불러올 때 필요한 변수들.
+	// join문으로 teamList, memberList를 불러올 때 필요한 변수들.
 	private int utSeq; //ut_seq
 	private String userId; //user_id
 	private int role;
 	private String nick;
-	
+	private String profileSrc; //profile_src
+	private String visible;
 }
--- src/main/java/com/allabo/vo/UsersTeamVO.java
+++ src/main/java/com/allabo/vo/UsersTeamVO.java
@@ -3,7 +3,7 @@ package com.allabo.vo;
 import lombok.Data;
 
 @Data
-public class UsersTeamVO {
+public class UsersTeamVO extends TeamAndMemberVO {
 	private String userId; // user_id
 	private int teamSeq; //team_seq;
 	private int role; //team_seq;
